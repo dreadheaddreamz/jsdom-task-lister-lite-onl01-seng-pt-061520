e13140137a5381024805fdc7eb430827eb627677
James Batsell <jbfuturedev@gmail.com>
2020-12-23 19:57:29 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form');
+  const formDate = document.getElementById('create-date-form');
+  let tasks = document.getElementById('tasks') 
+  let newToDo = document.getElementById('new-task-description');
+  form.addEventListener('submit', function(event) {
+   event.preventDefault();
+   let toDo = newToDo.value;
+   let toDoItem = document.createElement('li');
+   let toDoItemText = document.createTextNode(toDo);
+   toDoItem.appendChild(toDoItemText);
+   tasks.appendChild(toDoItem);
+   newToDo.value = "";
+});
 });
